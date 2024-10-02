deffe4ddb367cfbe5a322aa219b6437e506f75a7
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2024-10-02 19:11:57 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './src/**/*.{js,jsx@}', // For Vite or Next.js: adjust to match your file structure
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
